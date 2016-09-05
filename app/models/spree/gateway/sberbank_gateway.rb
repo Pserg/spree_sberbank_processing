74e7c5df4151a86eeeede94f7f1d51e88b5778c7
PSerg <pserg183@gmail.com>
2016-09-05 12:18:06 +0700

    add url method

--- app/models/spree/gateway/sberbank_gateway.rb
+++ app/models/spree/gateway/sberbank_gateway.rb
@@ -2,13 +2,27 @@ module Spree
   class Gateway::SberbankGateway < Gateway
 
     preference :api_username, :string
-    preference :api_password, :string
+    preference :api_password, :password
 
-    def authorize(money, credit_card, options = {})
+    attr_accessor :api_username, :api_password, :server, :test_mode
 
+    def provider_class
+      Spree::Gateway::SberbankGateway
+    end
+
+    def url
+      if :test_mode
+        'https://3dsec.sberbank.ru/payment/rest/'
+      else
+        'https://securepayments.sberbank.ru/payment/rest/'
+      end
     end
 
 
+
+    def purchase
+
+    end
 
   end
 end
