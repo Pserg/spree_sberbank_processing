d3678d48e00c2bac38a72b954ceaeecfb37e0f4c
PSerg <pserg183@gmail.com>
2016-09-13 21:36:07 +0700

    fix errors

--- app/controllers/spree/checkout_controller_decorator.rb
+++ app/controllers/spree/checkout_controller_decorator.rb
@@ -14,16 +14,19 @@ Spree::CheckoutController.class_eval do
                     'orderNumber' => "#{@order.number}-#{rand(1..9)}#{rand(1..9)}#{rand(1..9)}", 'returnUrl' => "http://#{params[:return_url]}", 'amount' => amount.to_i,
                     'order_id' => @order.id, 'payment_method_id' => params[:payment_method].to_i}
     response = payment_method.register_order(order_params)
-    redirect_to_payment_page if response.success?
-    protect_finalization
+    if response.success?
+      redirect_to_payment_page
+    else
+      protect_finalization
+    end
   end
 
   def redirect_to_payment_page
     @payment.state = 'pending'
     @payment.save
     if get_transaction
+      @order.save
       redirect_to get_transaction.form_url
-      return
     else
       protect_finalization
     end
@@ -32,17 +35,23 @@ Spree::CheckoutController.class_eval do
   def confirm_order
     return unless @order.state == 'payment' && @order.payments.first
     @payment = Spree::Payment.where(order_id: @order.id).first
-    return unless @payment.state = 'pending' || get_transaction.nil?
+    return unless @payment.state = 'pending' || get_transaction
     payment_method = Spree::PaymentMethod.where(id: @payment.payment_method_id).first
     order_params = {'userName' => payment_method.preferences[:api_username], 'password' => payment_method.preferences[:api_password],'orderId' => get_transaction.registered_order_id}
     response = payment_method.get_order_status(order_params)
-    if response['ErrorCode'] == '0' && response['OrderStatus'] == '2'
-      @payment.state = 'completed' && @payment.save
+
+    if response['ErrorCode'] == '0' && response['OrderStatus'] == 2
+      @payment.state = 'completed'
+      @payment.save
       @order.update(state: 'complete', completed_at: Time.now, payment_state: 'paid')
       @current_order = nil
       flash.notice = Spree.t(:order_processed_successfully)
       flash['order_completed'] = true
       redirect_to completion_route
+    elsif response['ErrorCode'] == '0' && response['OrderStatus'] == 0
+      flash.notice = "Заказ находится в процессе оплаты. Если вы случайно закрыли страницу оплаты
+                      пройдите по ссылке #{get_transaction.form_url}"
+      redirect_to checkout_state_path('payment') && return
     elsif response['ErrorCode'] != '0' && response['OrderStatus'] != '2'
       flash[:error] = "Ошибка обработки платежа: #{response['ErrorMessage']} "
       redirect_to checkout_state_path('payment') && return
